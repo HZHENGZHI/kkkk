753ffe0bc2ccb4c1b5abc381cd0f4b6510715931
wdnm_windows <1194365836@qq.com>
2019-12-18 16:19:25 +0800

    test

--- src/admin/quit_ticket.java
+++ src/admin/quit_ticket.java
@@ -161,7 +161,6 @@ public class quit_ticket {
                   } catch (Exception ex) {
                        ex.printStackTrace();
                     }
-
                }
            }});
     }
